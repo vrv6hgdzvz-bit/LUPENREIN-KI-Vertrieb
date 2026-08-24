c5a9b0edeb6e50d22d1a165643cebab7dc7ac4de
Lupenrein Vertrieb <vrv6hgdzvz@privaterelay.appleid.com>
2026-08-24 11:36:55 +0200

    feat: add customer self-service offer flow

---
+++ app/anfrage/page.tsx
@@ -0,0 +1,4 @@
+import CustomerQuestionnaireWizard from '@/components/CustomerQuestionnaireWizard'
+export const metadata={title:'Objektanfrage | LUPENREIN'}
+export default function Page(){return <CustomerQuestionnaireWizard/>}
+
